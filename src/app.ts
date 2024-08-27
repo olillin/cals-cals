e2bc1ce03bf841e95c97d32783d953759bc0b06e
Oli <oli@olillin.com>
2024-08-27 17:42:44 +0200

    Serve static files from public

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,17 @@ app.get('/', (req, res) => {
 })
 
 app.use('/vklass', new VklassAdapter().createRouter())
+if (fs.existsSync('public')) {
+    app.use('/', express.static('public', {
+        setHeaders: function(res, path) {
+            if (path.endsWith('.ics')) {
+                res.set('Cache-Control', 'no-store, max-age=0')
+            }
+        }
+    }))
+} else {
+    console.warn('WARNING: Could not find public directory, static files will not be served')
+}
 
 // Start server
 var server
