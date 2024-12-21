4cb5f306c44711c9a5d5875bcdd43906b3c4f1a9
Oli <oli@olillin.com>
2024-12-21 02:21:32 +0100

    Render calendar tree

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Response } from 'express'
 import fs, { readFile } from 'fs'
+import path from 'path'
 import https from 'https'
 import http from 'http'
 import VklassAdapter from './adapters/VklassAdapter'
@@ -24,11 +25,17 @@ const DEFAULT_ENVIRONMENT: Concrete<EnvironmentVariables> = {
 const ENVIRONMENT: Concrete<EnvironmentVariables> = Object.assign(Object.assign({}, DEFAULT_ENVIRONMENT), process.env as EnvironmentVariables)
 const { PORT } = ENVIRONMENT
 
+// Paths
+const REDIRECTS_PATH = '../data/redirects.json'
+const PICKER_PATH = '../data/picker.json'
+const PUBLIC_DIRECTORY = 'public'
+const CALENDAR_DIRECTORY = '../data/calendars'
+// const INDEX_PATH = '../data/calendars/index'
+
 // Setup express app
 const app = express()
 
 // Redirects
-const REDIRECTS_PATH = '../data/redirects.json'
 interface Redirects {
     [x: string]: string
 }
@@ -41,6 +48,11 @@ app.use((req, res, next) => {
     next()
 })
 
+// Picker
+app.get('/picker.json', (req, res) => {
+    res.sendFile(path.resolve(PICKER_PATH))
+})
+
 app.use('/vklass', new VklassAdapter().createRouter())
 
 function noStoreCals(res: Response, path: string) {
@@ -49,14 +61,12 @@ function noStoreCals(res: Response, path: string) {
     }
 }
 
-const PUBLIC_DIRECTORY = 'public'
 if (fs.existsSync(PUBLIC_DIRECTORY)) {
     app.use('/', express.static(PUBLIC_DIRECTORY))
 } else {
     console.warn('WARNING: No public directory')
 }
 
-const CALENDAR_DIRECTORY = '../data/calendars'
 if (fs.existsSync(CALENDAR_DIRECTORY)) {
     app.use(
         '/c',
@@ -85,64 +95,63 @@ if (fs.existsSync(CALENDAR_DIRECTORY)) {
 }
 
 // Merge calendars
-const INDEX_PATH = '../data/calendars/index'
-const calendarIndex = fs
-    .readFileSync(INDEX_PATH)
-    .toString()
-    .split('\n')
-    .map(line => line.trim())
-    .filter(line => line.length > 0)
-console.log('Available calendars:', calendarIndex)
+// const calendarIndex = fs
+//     .readFileSync(INDEX_PATH)
+//     .toString()
+//     .split('\n')
+//     .map(line => line.trim())
+//     .filter(line => line.length > 0)
+// console.log('Available calendars:', calendarIndex)
 
-function dec2bin(dec: number) {
-    return (dec >>> 0).toString(2)
-}
+// function dec2bin(dec: number) {
+//     return (dec >>> 0).toString(2)
+// }
 
-app.get('/m/:calendars', async (req, res) => {
-    const appendOriginName = !!(req.query['origin'] ?? false)
+// app.get('/m/:calendars', async (req, res) => {
+//     const appendOriginName = !!(req.query['origin'] ?? false)
 
-    const selectedCalendars = parseInt(req.params.calendars)
-    if (isNaN(selectedCalendars)) {
-        res.status(400).json({
-            error: {
-                message: 'Invalid calendars',
-            },
-        })
-        return
-    }
-    const selectedBits = dec2bin(selectedCalendars)
+//     const selectedCalendars = parseInt(req.params.calendars)
+//     if (isNaN(selectedCalendars)) {
+//         res.status(400).json({
+//             error: {
+//                 message: 'Invalid calendars',
+//             },
+//         })
+//         return
+//     }
+//     const selectedBits = dec2bin(selectedCalendars)
 
-    const calendarNames: string[] = calendarIndex.filter((_, i) => {
-        return selectedBits.length > i && selectedBits.charAt(selectedBits.length - 1 - i) === '1'
-    })
-    if (calendarNames.length == 0) {
-        res.status(400).json({
-            error: {
-                message: 'No calendars selected',
-            },
-        })
-        return
-    }
+//     const calendarNames: string[] = calendarIndex.filter((_, i) => {
+//         return selectedBits.length > i && selectedBits.charAt(selectedBits.length - 1 - i) === '1'
+//     })
+//     if (calendarNames.length == 0) {
+//         res.status(400).json({
+//             error: {
+//                 message: 'No calendars selected',
+//             },
+//         })
+//         return
+//     }
 
-    const calendars = await Promise.all(
-        calendarNames.map(
-            name =>
-                new Promise<IcalObject>((resolve, reject) =>
-                    fs.readFile(CALENDAR_DIRECTORY + '/' + name, (err, data) => {
-                        if (err) {
-                            reject(err)
-                        } else {
-                            const ical = ical2json.convert(data.toString())
-                            resolve(ical)
-                        }
-                    })
-                )
-        )
-    )
+//     const calendars = await Promise.all(
+//         calendarNames.map(
+//             name =>
+//                 new Promise<IcalObject>((resolve, reject) =>
+//                     fs.readFile(CALENDAR_DIRECTORY + '/' + name, (err, data) => {
+//                         if (err) {
+//                             reject(err)
+//                         } else {
+//                             const ical = ical2json.convert(data.toString())
+//                             resolve(ical)
+//                         }
+//                     })
+//                 )
+//         )
+//     )
 
-    const ical = ical2json.revert(mergeCalendarsIcal(calendars, appendOriginName))
-    res.status(200).end(ical)
-})
+//     const ical = ical2json.revert(mergeCalendarsIcal(calendars, appendOriginName))
+//     res.status(200).end(ical)
+// })
 
 // Start server
 var server
--- src/public/calendar-picker.ts
+++ src/public/calendar-picker.ts
@@ -1,11 +1,6 @@
 const indexUrl = '/c/index'
 const selectedClassName = 'selected'
 
-const elements: Promise<HTMLElement[]> = (async () => {
-    const index = await getIndex(indexUrl)
-    return toGridElements(index)
-})()
-
 interface PickerConfig {
     calendars: PickerCalendar[]
 }
@@ -32,22 +27,22 @@ const picker: Promise<PickerConfig> = (async () => {
 })()
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const calendarGrid = document.getElementById('calendar-grid') as HTMLDivElement
+    const calendarContainer = document.getElementById('calendars') as HTMLDivElement
 
     const calendarTree: PickerCalendarTree = {}
-    const addCalendar = (calendar: PickerCalendar) => {
+    function addCalendar(calendar: PickerCalendar) {
         if (calendar.category) {
-            let tokens: string[] = calendar.category.split('/')
+            let tokens: string[] = calendar.category.split('/').filter(token => token.trim().length)
             let latest: string
             let node: PickerCalendarTree = calendarTree
-            while (tokens) {
-                [latest, ...tokens] = tokens
-                let subcategory = node.subcategories?.find(s => {s.name === latest})
+            while (tokens.length > 0) {
+                ;[latest, ...tokens] = tokens
+                let subcategory = node.subcategories?.find(s => s.name === latest)
                 if (subcategory) {
                     node = subcategory
                 } else {
                     subcategory = {
-                        name: latest
+                        name: latest,
                     }
                     if (node.subcategories) {
                         node.subcategories.push(subcategory)
@@ -57,6 +52,10 @@ document.addEventListener('DOMContentLoaded', async () => {
                     node = subcategory
                 }
             }
+            if (!node.calendars) {
+                node.calendars = []
+            }
+            node.calendars!.push(calendar)
         } else {
             if (calendarTree.calendars) {
                 calendarTree.calendars.push(calendar)
@@ -70,61 +69,110 @@ document.addEventListener('DOMContentLoaded', async () => {
         addCalendar(calendar)
     })
 
-    const renderRoot = calendarGrid
-    const renderTree = (tree: PickerCalendarTree|PickerCalendarSubTree, level=2) => {
-        if ("name" in tree) {
-            const header = document.createElement(`h${level}`)
-            renderRoot.append(header)
-        }
-
-        const sortedCalendars = tree.calendars?.sort((a, b) => a.filename.localeCompare(b .filename))
-        sortedCalendars.forEach(calendar => {
-            
-        })
-    }
-
-    ;(await elements).forEach(element => {
-        calendarGrid.append(element)
-
-        element.addEventListener('click', ev => {
-            const el = ev.currentTarget as HTMLElement
-            if (el.classList.contains(selectedClassName)) {
-                el.classList.remove(selectedClassName)
-            } else {
-                el.classList.add(selectedClassName)
-            }
-            update()
-        })
-    })
-
+    renderTree(calendarTree, calendarContainer)
     update()
 
-    const aspaAlltButton = document.getElementById('aspa-allt') as HTMLInputElement 
-    aspaAlltButton.addEventListener("change", () => {
-        if (aspaAlltButton.checked) {
-            aspaAllt()
-        } else {
-            select(() => false)
-        }
-    })
+    // Make checkbox fields clickable anywhere
+    for (const field of document.getElementsByClassName('checkbox-field')) {
+        const checkbox = field.getElementsByTagName('input')[0]
+        field.addEventListener('click', ev => {
+            if (ev.target !== checkbox) {
+                ev.preventDefault()
+                checkbox.click()
+            }
+        })
+    }
 })
 
-async function toGridElements(index: string[]): Promise<HTMLElement[]> {
-    const elementPromises = index.map(async (filename, i) => {
-        const displayName = await getCalendarName(filename)
+function renderTree(tree: PickerCalendarTree | PickerCalendarSubTree, container: HTMLElement, depth: number = 2) {
+    if (depth > 6) {
+        throw Error(`Unable to create header for depth ${depth}`)
+    }
 
+    const subContainer = document.createElement('div')
+    subContainer.className = 'calendar-group'
+
+    if ('name' in tree) {
+        const header = document.createElement(`h${depth}`)
+        header.innerText = tree.name
+        subContainer.append(header)
+    }
+
+    // Select all button
+    const selectAll = createSelectAllButton(subContainer)
+    subContainer.appendChild(selectAll)
+
+    // Render calendar buttons
+    const subCalendars = tree.calendars ?? []
+    const sortedCalendars = subCalendars //
+        .sort((a, b) => a.filename.localeCompare(b.filename))
+        .sort((a, b) => (a.order ?? Infinity) - (b.order ?? Infinity))
+
+    const calendarDiv = document.createElement('div')
+    calendarDiv.className = 'calendar-grid'
+    subContainer.appendChild(calendarDiv)
+    sortedCalendars.forEach(calendar => {
         const button = document.createElement('button')
-        button.classList.add('calendar-item')
-        button.setAttribute('data-bit-value', (2 ** i).toString())
-        button.setAttribute("data-calendar-filename", filename)
-        const text = document.createElement('span')
-        text.innerText = displayName
-        button.appendChild(text)
-        return button
+        makeCalendarButton(button, calendar)
+        calendarDiv.appendChild(button)
+    })
+    container.appendChild(subContainer)
+
+    // Render subcategories
+    const subCategories = tree.subcategories ?? []
+    const sortedCategories = subCategories.sort((a, b) => a.name.localeCompare(b.name))
+    sortedCategories.forEach(category => {
+        renderTree(category, subContainer, depth + 1)
+    })
+}
+
+function createSelectAllButton(group: HTMLElement) {
+    const selectAll = document.createElement('span')
+    selectAll.className = 'checkbox-field'
+    const selectAllCheckbox = document.createElement('input')
+    selectAllCheckbox.type = 'checkbox'
+    selectAllCheckbox.className = 'select-all'
+    const selectAllLabel = document.createElement('label')
+    selectAllLabel.innerText = 'Select all'
+
+    selectAll.appendChild(selectAllCheckbox)
+    selectAll.appendChild(selectAllLabel)
+
+    selectAllCheckbox.addEventListener('change', ev => {
+        if (selectAllCheckbox.checked) {
+            select(el => group.contains(el))
+        } else {
+            unselect(el => group.contains(el))
+        }
     })
 
-    const elements = await Promise.all(elementPromises)
-    return elements
+    return selectAll
+}
+
+async function makeCalendarButton(button: HTMLButtonElement, calendar: PickerCalendar) {
+    // Data
+    button.setAttribute('data-calendar-id', calendar.id.toString())
+    button.setAttribute('data-calendar-filename', calendar.filename)
+
+    // Style
+    button.classList.add('calendar-item')
+
+    const text = document.createElement('span')
+    const displayName = await getCalendarName(calendar.filename)
+    text.innerText = displayName
+
+    // Select on click
+    button.addEventListener('click', ev => {
+        const el = ev.currentTarget as HTMLElement
+        if (el.classList.contains(selectedClassName)) {
+            el.classList.remove(selectedClassName)
+        } else {
+            el.classList.add(selectedClassName)
+        }
+        update()
+    })
+
+    button.appendChild(text)
 }
 
 function getIndex(indexUrl: string): Promise<string[]> {
@@ -162,22 +210,24 @@ function getCalendarName(filename: string): Promise<string> {
 }
 
 function update() {
-    const calendarGrid = document.getElementById('calendar-grid') as HTMLDivElement
+    // Get elements
+    const calendarContainer = document.getElementById('calendars') as HTMLDivElement
     const calendarUrl = document.getElementById('calendar-url') as HTMLInputElement
     const showOrigin = document.getElementById('show-origin') as HTMLInputElement
-    const aspaAlltCheck = document.getElementById('aspa-allt') as HTMLInputElement
 
     const showOriginSection = document.getElementById('show-origin-section') as HTMLSpanElement
     showOriginSection.hidden = true
     const calendarUrlSection = document.getElementById('calendar-url-section') as HTMLSpanElement
     calendarUrlSection.hidden = false
 
+    // Get domain name
     const urlBase = window.location.origin
 
-    const elements = Array.from(calendarGrid.getElementsByClassName('calendar-item'))
-    const selectedElements = Array.from(calendarGrid.getElementsByClassName(selectedClassName))
+    // Find selected elements
+    const elements = Array.from(calendarContainer.getElementsByClassName('calendar-item'))
+    const selectedElements = Array.from(calendarContainer.getElementsByClassName(selectedClassName))
     if (selectedElements.length == 1) {
-        const filename = selectedElements[0].getAttribute("data-calendar-filename")
+        const filename = selectedElements[0].getAttribute('data-calendar-filename')
 
         calendarUrl.value = `${urlBase}/c/${filename}`
     } else if (selectedElements.length > 1) {
@@ -185,7 +235,8 @@ function update() {
 
         let selected = 0
         for (const element of selectedElements) {
-            selected += parseInt(element.getAttribute('data-bit-value')!)
+            const id = parseInt(element.getAttribute('data-calendar-id')!)
+            selected += 1 << id
         }
         calendarUrl.value = `${urlBase}/m/${selected}?origin=${+showOrigin.checked}`
     } else {
@@ -193,11 +244,24 @@ function update() {
         calendarUrl.value = ''
     }
 
-    aspaAlltCheck.checked = elements.every(e => {
-        let isSelected = e.classList.contains('selected')
-        let isAspa = e.getAttribute('data-calendar-filename')!.startsWith('aspa')
-        return !(+isSelected ^ +isAspa)
-    })
+    for (const selectAll of document.getElementsByClassName('select-all')) {
+        ;(selectAll as HTMLInputElement).checked = elements.every(e => {
+            let isSelected = e.classList.contains('selected')
+            let inGroup = selectAll.parentElement!.parentElement!.contains(e)
+            console.log({
+                selectAll,
+                e,
+                isSelected,
+                inGroup,
+            })
+            return !(!isSelected && inGroup)
+            // S G
+            // 0 0 1
+            // 1 0 1
+            // 0 1 0
+            // 1 1 1
+        })
+    }
 }
 
 function copyUrl() {
@@ -215,21 +279,24 @@ function copyUrl() {
     }, 3000)
 }
 
-function aspaAllt() {
-    select(el => {
-        const filename = el.getAttribute('data-calendar-filename')!
-        return filename.startsWith("aspa")
-    })
-}
-
 function select(predicate: (el: Element) => boolean) {
-    const calendarGrid = document.getElementById('calendar-grid') as HTMLDivElement
-    
-    const elements = calendarGrid.getElementsByClassName("calendar-item")
+    const calendarContainer = document.getElementById('calendars') as HTMLDivElement
+
+    const elements = calendarContainer.getElementsByClassName('calendar-item')
     for (const element of elements) {
         if (predicate(element)) {
             element.classList.add(selectedClassName)
-        } else {
+        }
+    }
+    update()
+}
+
+function unselect(predicate: (el: Element) => boolean) {
+    const calendarContainer = document.getElementById('calendars') as HTMLDivElement
+
+    const elements = calendarContainer.getElementsByClassName('calendar-item')
+    for (const element of elements) {
+        if (predicate(element)) {
             element.classList.remove(selectedClassName)
         }
     }
