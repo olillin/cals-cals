a6e6853080a796c9d8c7faba45111507bb6b6a30
Oli <oli@olillin.com>
2026-03-03 01:20:30 +0100

    Update page footer with source code link

---
+++ app/ui/PageFooter.tsx
@@ -0,0 +1,30 @@
+export default function PageFooter() {
+    return (
+        <footer>
+            <span>
+                Made with <span className="pixelnheart"></span> by{' '}
+                <a href="https://wiki.chalmers.it/Cal" target="_blank">
+                    Cal
+                </a>
+                .
+            </span>
+            <span>
+                Is something wrong or could be improved?&nbsp;
+                <a
+                    href="https://github.com/olillin/cals-cals/issues"
+                    target="_blank"
+                >
+                    Leave an issue
+                </a>{' '}
+                on GitHub!
+            </span>
+            <span>
+                See the{' '}
+                <a href="https://github.com/olillin/cals-cals" target="_blank">
+                    source code
+                </a>{' '}
+                on GitHub.
+            </span>
+        </footer>
+    )
+}
